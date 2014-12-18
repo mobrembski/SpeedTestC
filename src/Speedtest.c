5ae425b3d7f2194b9f674aa8cc82ef19c519161c
Michał Obrembski <byku@byku.com.pl>
2014-12-18 18:02:47 +0100

    Added selecting best server by sorting array.

--- src/Speedtest.c
+++ src/Speedtest.c
@@ -7,6 +7,11 @@
 #include "SpeedtestConfig.h"
 #include "SpeedtestServers.h"
 
+int sortServers (SPEEDTESTSERVER_T **srv1, SPEEDTESTSERVER_T **srv2)
+{
+	return((*srv1)->distance - (*srv2)->distance);
+}
+
 int main(int argc, char **argv)
 {
     SPEEDTESTCONFIG_T *speedTestConfig=getConfig();
@@ -17,16 +22,18 @@ int main(int argc, char **argv)
     SPEEDTESTSERVER_T **serverList = getServers(&serverCount);
 
     printf("Grabbed %d servers\n",serverCount);
-    for(i=0;i<serverCount;i++) {
+    for(i=0;i<serverCount;i++)
     	serverList[i]->distance = haversineDistance(speedTestConfig->lat,
     									speedTestConfig->lon,
     									serverList[i]->lat,
     									serverList[i]->lon);
-        printf("Server [%d] URL: %s\n\t Name:%s Country: %s Sponsor: %s Dist: %ld\n",
-            i+1,serverList[i]->url,serverList[i]->name,serverList[i]->country,
-            serverList[i]->sponsor,serverList[i]->distance);
 
-    }
+    qsort (serverList,serverCount,sizeof(SPEEDTESTSERVER_T *),
+                (int (*)(const void *,const void *)) sortServers);
+
+    printf("Best Server URL: %s\n\t Name:%s Country: %s Sponsor: %s Dist: %ld\n",
+	    serverList[0]->url,serverList[0]->name,serverList[0]->country,
+	    serverList[0]->sponsor,serverList[0]->distance);
 	return 0;
 }
 
