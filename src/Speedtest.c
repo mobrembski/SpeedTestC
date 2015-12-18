c8ee0c8e6b38070cc0b0359d59c636dfbd520523
Michał Obrembski <byku@byku.com.pl>
2015-12-18 15:27:09 +0100

    Fix memleaks and some little memory optimization

--- src/Speedtest.c
+++ src/Speedtest.c
@@ -55,6 +55,48 @@ void parseCmdLine(int argc, char **argv) {
 
 void freeMem()
 {
+    free(downloadUrl);
+    free(uploadUrl);
+    free(serverList);
+    free(speedTestConfig);
+}
+
+void getBestServer()
+{
+    speedTestConfig = getConfig();
+    if (speedTestConfig == NULL)
+    {
+        printf("Cannot download speedtest.net configuration. Something is wrong...\n");
+        freeMem();
+        exit(1);
+    }
+    printf("Your IP: %s And ISP: %s\n",
+                speedTestConfig->ip, speedTestConfig->isp);
+    printf("Lat: %f Lon: %f\n", speedTestConfig->lat, speedTestConfig->lon);
+    serverList = getServers(&serverCount);
+    printf("Grabbed %d servers\n",serverCount);
+    if (serverCount == 0)
+    {
+        printf("Cannot download any speedtest.net server. Something is wrong...\n");
+        freeMem();
+        exit(1);
+    }
+    for(i=0;i<serverCount;i++)
+        serverList[i]->distance = haversineDistance(speedTestConfig->lat,
+            speedTestConfig->lon,
+            serverList[i]->lat,
+            serverList[i]->lon);
+
+    qsort(serverList,serverCount,sizeof(SPEEDTESTSERVER_T *),
+                (int (*)(const void *,const void *)) sortServers);
+
+    printf("Best Server URL: %s\n\t Name:%s Country: %s Sponsor: %s Dist: %ld\n",
+        serverList[0]->url,serverList[0]->name,serverList[0]->country,
+        serverList[0]->sponsor,serverList[0]->distance);
+    downloadUrl = getServerDownloadUrl(serverList[0]->url);
+    uploadUrl = malloc(sizeof(char) * strlen(serverList[0]->url) + 1);
+    strcpy(uploadUrl,serverList[0]->url);
+
     for(i=0;i<serverCount;i++){
         free(serverList[i]->url);
         free(serverList[i]->name);
@@ -62,78 +104,44 @@ void freeMem()
         free(serverList[i]->country);
         free(serverList[i]);
     }
-    free(downloadUrl);
-    free(uploadUrl);
-    free(serverList);
-    free(speedTestConfig);
 }
 
-int main(int argc, char **argv)
+void testDownload(const char *url)
 {
-    speedTestConfig = NULL;
-    parseCmdLine(argc,argv);
-    if(downloadUrl == NULL) {
-        speedTestConfig = getConfig();
-        if (speedTestConfig == NULL)
-        {
-            printf("Cannot download speedtest.net configuration. Something is wrong...\n");
-            freeMem();
-            exit(1);
-        }
-        printf("Your IP: %s And ISP: %s\n",
-                    speedTestConfig->ip, speedTestConfig->isp);
-        printf("Lat: %f Lon: %f\n", speedTestConfig->lat, speedTestConfig->lon);
-        serverList = getServers(&serverCount);
-        printf("Grabbed %d servers\n",serverCount);
-        if (serverCount == 0)
-        {
-            printf("Cannot download any speedtest.net server. Something is wrong...\n");
-            freeMem();
-            exit(1);
-        }
-        for(i=0;i<serverCount;i++)
-            serverList[i]->distance = haversineDistance(speedTestConfig->lat,
-                speedTestConfig->lon,
-                serverList[i]->lat,
-                serverList[i]->lon);
-
-        qsort(serverList,serverCount,sizeof(SPEEDTESTSERVER_T *),
-                    (int (*)(const void *,const void *)) sortServers);
-
-        printf("Best Server URL: %s\n\t Name:%s Country: %s Sponsor: %s Dist: %ld\n",
-            serverList[0]->url,serverList[0]->name,serverList[0]->country,
-            serverList[0]->sponsor,serverList[0]->distance);
-        downloadUrl = getServerDownloadUrl(serverList[0]->url);
-        uploadUrl = serverList[0]->url;
-    }
-    else {
-        uploadUrl = downloadUrl;
-        downloadUrl = getServerDownloadUrl(downloadUrl);
-    }
-
     /* Testing download... */
-    sockId = httpGetRequestSocket(downloadUrl);
+    sockId = httpGetRequestSocket(url);
     if(sockId == 0)
-        return 1;
+    {
+        printf("Unable to open socket for Download!");
+        freeMem();
+        exit(1);
+    }
     size = -1;
     totalTransfered = 0;
     gettimeofday(&tval_start, NULL);
     while(size!=0)
     {
-    	size = httpRecv(sockId,buffer,1500);
-		totalTransfered += size;
+        size = httpRecv(sockId,buffer,1500);
+        totalTransfered += size;
     }
     elapsedSecs = getElapsedTime(tval_start);
     speed = (totalTransfered/elapsedSecs)/1024;
     httpClose(sockId);
     printf("Bytes %lu downloaded in %.2f seconds %.2f kB/s\n",
         totalTransfered,elapsedSecs,speed);
+}
 
+void testUpload(const char *url)
+{
     /* Testing upload... */
     totalTransfered = totalToBeTransfered;
     sockId = httpPutRequestSocket(uploadUrl,totalToBeTransfered);
     if(sockId == 0)
-        return 1;
+    {
+        printf("Unable to open socket for Upload!");
+        freeMem();
+        exit(1);
+    }
     gettimeofday(&tval_start, NULL);
     while(totalTransfered != 0)
     {
@@ -150,6 +158,29 @@ int main(int argc, char **argv)
     httpClose(sockId);
     printf("Bytes %lu uploaded in %.2f seconds %.2f kB/s\n",
         totalToBeTransfered,elapsedSecs,speed);
+}
+
+int main(int argc, char **argv)
+{
+    speedTestConfig = NULL;
+    parseCmdLine(argc,argv);
+
+    if(downloadUrl == NULL)
+    {
+        getBestServer();
+    }
+    else
+    {
+        uploadUrl = downloadUrl;
+        tmpUrl = malloc(sizeof(char) * strlen(downloadUrl) + 1);
+        strcpy(tmpUrl,downloadUrl);
+        downloadUrl = getServerDownloadUrl(tmpUrl);
+        free(tmpUrl);
+    }
+
+    testDownload(downloadUrl);
+    testUpload(uploadUrl);
+
     freeMem();
     return 0;
 }
